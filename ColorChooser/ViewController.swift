d8871edd3a4b4918f6001678d932d5aceaa1e36c
Korchak Konstantin <korchak.konstantin@gmail.com>
2022-03-06 17:17:11 +0300

    logic setup

--- ColorChooser/ViewController.swift
+++ ColorChooser/ViewController.swift
@@ -8,12 +8,46 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
+    @IBOutlet var colorView: UIView!
+    
+    @IBOutlet var redValueLabel: UILabel!
+    @IBOutlet var greenValueLabel: UILabel!
+    @IBOutlet var blueValueLabel: UILabel!
+    
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+    
+    private func setBackgroundColor() {
+        colorView.backgroundColor = UIColor(
+            red: CGFloat(redSlider.value),
+            green: CGFloat(greenSlider.value),
+            blue: CGFloat(blueSlider.value),
+            alpha: 1.0
+        )
     }
-
-
+    
+    override func viewDidLoad() {
+        colorView.layer.cornerRadius = 10
+        redValueLabel.text = String(redSlider.value)
+        greenValueLabel.text = String(greenSlider.value)
+        blueValueLabel.text = String(blueSlider.value)
+        setBackgroundColor()
+    }
+    
+    @IBAction func redSliderChanger() {
+        redValueLabel.text = String(round(redSlider.value * 100) / 100)
+        setBackgroundColor()
+    }
+    @IBAction func greenSliderChanger() {
+        greenValueLabel.text = String(round(greenSlider.value * 100) / 100)
+        setBackgroundColor()
+    }
+    @IBAction func blueSliderChanger() {
+        blueValueLabel.text = String(round(blueSlider.value * 100) / 100)
+        setBackgroundColor()
+    }
+    
 }
 
